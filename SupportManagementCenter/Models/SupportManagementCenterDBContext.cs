4e461ddd30999f323591f2b44cf03e906b2d19ab
Sean McCammon <sean_mccammon@hotmail.com>
2020-04-26 18:04:03 +0100

    Started adding DB context requirements

---
+++ SupportManagementCenter/Models/SupportManagementCenterDBContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace SupportManagementCenter.Models
+{
+    public class SupportManagementCenterDBContext : DbContext
+    {
+        public SupportManagementCenterDBContext(DbContextOptions<SupportManagementCenterDBContext> options):base(options)
+        {
+            
+        }
+
+        public DbSet<SupportTicketModel> supportTickets { get; set; }
+    }
+}
